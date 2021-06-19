9b05f62343e3a6b444af916bcbe058ec571115ab
fiatjaf <fiatjaf@alhur.es>
2021-06-18 21:29:52 -0300

    rework note rendering.

--- src/components/NoteCard.svelte
+++ src/components/NoteCard.svelte
@@ -1,6 +1,5 @@
 <script>
   import {fly} from 'svelte/transition'
-  import {push} from 'svelte-spa-router'
 
   import {publish} from '../lib/relay'
   import {getMetadataStore} from '../lib/metadata'
@@ -8,6 +7,7 @@
   import ProfileName from './ProfileName.svelte'
 
   export let note
+  export let ref = false
 
   let inReplyTo = note.tags.find(
     ([tag, id]) => tag === 'e' && typeof id === 'string'
@@ -15,7 +15,7 @@
   let replying = false
   let replyMsg = ''
 
-  $: author = getMetadataStore(note.pubkey)
+  $: author = getMetadataStore(note && note.pubkey)
 
   const sendReply = id => {
     try {
@@ -32,34 +32,38 @@
   }
 </script>
 
-<article class="card p-5" in:fly={{y: 200, duration: 300}}>
+<article class="card p-3" class:ref in:fly={{y: 200, duration: 300}}>
   <header class="card-header is-shadowless">
     <div class="media">
-      <figure class="media-left is-clickable">
-        <img
-          alt="~"
-          class="is-48x48 image"
-          on:click={() => push(`#/u/${note.pubkey}`)}
-          src={$author.picture ||
-            'https://bulma.io/images/placeholders/128x128.png'}
-        />
+      <figure class="media-left">
+        <a href={`#/u/${note.pubkey}`}>
+          <img
+            alt="~"
+            class="image"
+            class:is-48x48={!ref}
+            class:is-24x24={ref}
+            src={$author.picture ||
+              'https://bulma.io/images/placeholders/128x128.png'}
+          />
+        </a>
       </figure>
       <div class="media-content">
         <div class="content">
           <p>
             <ProfileName meta={$author} />
-            <strong
-              class="is-clickable abbr"
-              on:click={() => push(`#/u/${note.pubkey}`)}
-            >
-              {note.pubkey}
-            </strong>
-            <small class="is-clickable" on:click={() => push(`#/n/${note.id}`)}>
-              {humanDate(note.created_at)}
+            <a class="abbr" href={`#/u/${note.pubkey}`}>
+              <strong>
+                {note.pubkey}
+              </strong>
+            </a>
+            <small>
+              <a href={`#/n/${note.id}`}>
+                {humanDate(note.created_at)}
+              </a>
             </small>
             <br />
             {#if inReplyTo}
-              <a href={`#/n/${inReplyTo}`}>
+              <a href={`#/n/${inReplyTo[1]}`}>
                 <small>In reply to {abbr(inReplyTo[1])}</small>
               </a>
             {/if}
@@ -73,54 +77,69 @@
       {note.content}
     </div>
   </div>
-  <footer class="card-footer">
-    <span
-      class="icon is-medium"
-      on:click={() => (replying = !replying)}
-      title="Reply"
-    >
-      <i class="icon ion-md-chatboxes" />
-    </span>
-    {#if replying}
-      <div class="field is-grouped">
-        <p class="control is-expanded">
-          <textarea
-            class="textarea reply"
-            placeholder="Send a reply..."
-            bind:value={replyMsg}
-          />
-        </p>
-        <p class="control">
-          <a
-            href="_"
-            class="button is-ghost"
-            on:click|preventDefault={() => sendReply(note.id)}
-          >
-            Reply
-          </a>
-        </p>
-      </div>
-    {/if}
-  </footer>
+  {#if !ref}
+    <footer class="card-footer">
+      <span
+        class="icon is-medium"
+        on:click={() => (replying = !replying)}
+        title="Reply"
+      >
+        <i class="icon ion-md-chatboxes" />
+      </span>
+      {#if replying}
+        <div class="field is-grouped">
+          <p class="control is-expanded">
+            <textarea
+              class="textarea reply"
+              placeholder="Send a reply..."
+              bind:value={replyMsg}
+            />
+          </p>
+          <p class="control">
+            <button
+              class="button"
+              on:click|preventDefault={() => sendReply(note.id)}
+            >
+              Reply
+            </button>
+          </p>
+        </div>
+      {/if}
+    </footer>
+  {/if}
 </article>
 
 <style>
   article {
     margin-bottom: 2rem;
   }
+
+  a {
+    color: inherit;
+  }
+
+  .ref {
+    box-shadow: none;
+    margin: 0 20px;
+    background-color: #f8f8f8;
+    border: 1px dotted black;
+    font-size: 70%;
+  }
+
   .card-footer {
     display: flex;
     align-items: center;
     border: none;
     justify-content: flex-end;
   }
-
   .card-footer > span {
     cursor: pointer;
   }
-
   .card-content {
     word-break: break-all;
+  }
+  .ref .card-content {
+    padding: 0.25rem;
   }
 
   :global(.card-footer > .field) {
--- src/routes/Note.svelte
+++ src/routes/Note.svelte
@@ -1,22 +1,25 @@
 <script>
-  import {onMount} from 'svelte'
+  import {onDestroy} from 'svelte'
   import {SortedMap} from 'insort'
-  import {push} from 'svelte-spa-router'
   import {pool} from '../lib/relay'
-  import {abbr, humanDate} from '../lib/helpers'
   import NoteCard from '../components/NoteCard.svelte'
 
   export let params
 
   let s, note, comments, refs
-  let replyMsg = ''
-
-  const sendReply = () => {
-    return
-  }
 
   let basicFilter = [{id: params.note_id}, {'#e': params.note_id}]
   let hasReceivedNote = false
+
+  $: {
+    console.log('note changed', params.note_id)
+    if (s) s.unsub()
+    if (params.note_id) getNote()
+  }
+
+  onDestroy(() => {
+    if (s) s.unsub()
+  })
 
   function getNote() {
     let cmp = (a, b) => b.split(':')[1] - a.split(':')[1]
@@ -31,92 +34,43 @@
             hasReceivedNote = true
             s.sub({
               filter: basicFilter.concat(
-                note.tags.filter(([t]) => t === 'e').map(([_, v]) => v)
+                note.tags.filter(([t]) => t === 'e').map(([_, v]) => ({id: v}))
               )
             })
           }
         } else if (
-          event.id.tags.findIndex(([_, v]) => v === params.note_id) !== -1
+          event.tags.findIndex(([_, v]) => v === params.note_id) !== -1
         ) {
           comments.set(event.id + ':' + event.created_at, event)
-        } else {
+          comments = comments
+        } else if (
+          note &&
+          note.tags.findIndex(([_, v]) => v === event.id) !== -1
+        ) {
           refs.set(event.id + ':' + event.created_at, event)
+          refs = refs
         }
       },
       filter: basicFilter
     })
   }
-
-  onMount(() => {
-    getNote()
-    return () => {
-      if (s) s.unsub()
-    }
-  })
 </script>
 
 <section>
   {#if refs}
     <div class="block">
       {#each Array.from(refs.values()) as ref}
-        <NoteCard note={ref} />
+        <NoteCard note={ref} ref />
       {/each}
     </div>
   {/if}
   {#if note}
-    <article class="card p-5 my-5">
-      <div class="card-header is-shadowless">
-        <div class="media">
-          <figure class="media-left pointer">
-            <p class="image is-32x32">
-              <img
-                alt=""
-                on:click={() => push(`#/u/${note.pubkey}`)}
-                class="is-rounded"
-                src="https://bulma.io/images/placeholders/128x128.png"
-              />
-            </p>
-          </figure>
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong
-                  class="pointer"
-                  on:click={() => push(`#/u/${note.pubkey}`)}
-                >
-                  {abbr(note.pubkey)}
-                </strong>
-                <br />
-                <small>{humanDate(note.created_at)}</small>
-                <br />
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card-content">
-        {note.content}
-        <div class="post my-5">
-          <div class="block">
-            <textarea
-              class="textarea"
-              placeholder="Send a comment..."
-              bind:value={replyMsg}
-            />
-          </div>
-        </div>
-      </div>
-      <div class="card-footer is-justify-content-flex-end">
-        <button class="button is-primary is-rounded" on:click={sendReply}
-          >Send</button
-        >
-      </div>
-    </article>
+    <NoteCard {note} />
   {/if}
   {#if comments}
     <div class="block">
       {#each Array.from(comments.values()) as comment}
-        <NoteCard note={comment} />
+        <NoteCard note={comment} ref />
       {/each}
     </div>
   {/if}
